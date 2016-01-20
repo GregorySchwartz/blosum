dbc5892d63dbf5c6569848c0fb57bca5ab4c88d6
Gregory Schwartz <gs394@drexel.edu>
2016-01-20 16:57:28 -0500

    Clustering done

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,71 @@
+{- Main
+by Gregory Schwartz
+
+-- | Computes a BLOSUM matrix, but takes into account alternate amino acids
+-}
 module Main where
 
-import Lib
+-- Standard
+import Data.Maybe
+import qualified System.IO as IO
+import qualified Data.Sequence as Seq
+
+-- Cabal
+import Options.Applicative
+import Data.Fasta.Text
+import Pipes
+import qualified Pipes.Prelude as P
+import qualified Pipes.Text.IO as PT
+
+-- Local
+import Types
+import Cluster
+
+-- Command line arguments
+data Options = Options { input    :: Maybe String
+                       , identity :: Double
+                       , output   :: Maybe String
+                       }
+
+-- Command line options
+options :: Parser Options
+options = Options
+      <$> optional ( strOption
+          ( long "input"
+         <> short 'i'
+         <> metavar "FILE"
+         <> help "The input fasta file with blocks labeled in a field"
+          )
+        )
+      <*> option auto
+          ( long "identity"
+         <> short 'd'
+         <> metavar "[100] | PERCENT"
+         <> value 100
+         <> help "The percent identity of two sequences for clustering.\
+                 \ BLOSUM's most famous identity is 62" )
+      <*> optional ( strOption
+          ( long "output"
+         <> short 'o'
+         <> metavar "FILE"
+         <> help "The output file containing the matrix"
+          )
+        )
+
+blossum :: Options -> IO ()
+blossum opts = do
+    h <- case input opts of
+            Nothing  -> return IO.stdin
+            (Just x) -> IO.openFile x IO.ReadMode
+    fastaList <- runEffect $ P.toListM $ pipesFasta . PT.fromHandle $ h
+
+    print $ clusterIdentity (Identity $ identity opts) . Seq.fromList $ fastaList
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= blossum
+  where
+    opts = info (helper <*> options)
+      ( fullDesc
+     <> progDesc "Computes a BLOSUM matrix, but takes into account\
+                 \ alternate amino acids"
+      )
--- src/Cluster.hs
+++ src/Cluster.hs
@@ -5,48 +5,75 @@ by Gregory Schwartz
 distance identity
 -}
 
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ViewPatterns #-}
+
 module Cluster
     ( clusterIdentity
+    , groupBy'
     ) where
 
 -- Standard
-import qualified Data.Set.Monad as Set
+import qualified Data.Map as Map
+import qualified Data.Sequence as Seq
+import qualified Data.Foldable as F
 
 -- Cabal
+import qualified Data.Text as T
 import Data.Fasta.Text
-import Math.Diversity.Diversity (hamming)
+
+-- Local
+import Types
+
+-- | Like hamming, but similarity rather than distance
+negHamming :: T.Text -> T.Text -> Double
+negHamming x = sum . map (\(x, y) -> if x == y then 1 else 0) . T.zip x
 
 -- | Takes in an identity and fasta sequences and returns the sequences
 -- grouped together by hamming distance identity
 clusterIdentity :: Identity
-                -> [FastaSequence]
-                -> Map.Map Int (Set.Set FastaSequence)
-clusterIdentity identity = 
+                -> Seq.Seq FastaSequence
+                -> Map.Map Int (Seq.Seq FastaSequence)
+clusterIdentity identity = Map.fromList
+                         . zip [1..]
+                         . clusterIdentityGo identity
 
 -- | Keep comparing clusters until no more fusions (no change in size) make
 -- sense
-clusterIdentityGo :: Bool
-                  -> Identity
-                  -> [Set.Set FastaSequence]
-                  -> [Set.Set FastaSequence]
-compareGroups identity xs
-    | 
-  where
-    before = Map.size xs
+clusterIdentityGo :: Identity
+                  -> Seq.Seq FastaSequence
+                  -> [Seq.Seq FastaSequence]
+clusterIdentityGo identity = F.toList
+                           . groupBy' (compareSeqs identity)
 
--- | Either the groups are joined by identity or not
-compareGroups :: Identity
-              -> Seq.Set FastaSequence
-              -> Seq.Set FastaSequence
-              -> Maybe Seq.Set FastaSequence
-compareGroups identity xs ys
-    | any $ (\x y -> getIdentity x y >= identity) <$> xs <*> ys =
-        xs Set.union ys
-    | otherwise                                                 = Nothing
+-- | Group together by all pairings rather than adjacent. Altered from
+-- lyxia's original to use sequences.
+groupBy' :: (a -> a -> Bool) -> Seq.Seq a -> Seq.Seq (Seq.Seq a)
+groupBy' _ (Seq.null -> True) = Seq.empty
+groupBy' f (Seq.viewl -> x Seq.:< xs) = eqX Seq.<| groupBy' f neqX
+  where
+    (!eqX, !neqX) = eqTo f Seq.empty (Seq.singleton x) xs
+
+-- | Helper to groupBy'. Altered from lyxia's original to use sequences.
+eqTo :: (a -> a -> Bool)
+     -> Seq.Seq a
+     -> Seq.Seq a
+     -> Seq.Seq a
+     -> (Seq.Seq a, Seq.Seq a)
+eqTo _ acc (Seq.null -> True) zs = (acc, zs)
+eqTo f acc (Seq.viewl -> x Seq.:< xs) zs =
+    eqTo f (x Seq.<| acc) (eqX Seq.>< xs) neqX
+  where
+    (!eqX, !neqX) = Seq.partition (f x) zs
+
+-- | Either the sequences are similar or not
+compareSeqs :: Identity -> FastaSequence -> FastaSequence -> Bool
+compareSeqs identity x y = getIdentity x y >= identity
 
 -- | Get the identity between two sequences
 getIdentity :: FastaSequence -> FastaSequence -> Identity
-getIdentity xs ys = (* 100)
-                  . (/ T.length xs)
-                  . hamming (fastaSeq xs)
+getIdentity xs ys = Identity
+                  . (* 100)
+                  . (/ (fromIntegral . T.length . fastaSeq $ xs))
+                  . negHamming (fastaSeq xs)
                   $ (fastaSeq ys)
